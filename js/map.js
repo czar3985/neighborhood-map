d1660a914ceaccedf16be293321bed11eab1b5d3
Rina Lingad <pixie.czar@gmail.com>
2018-10-20 18:43:22 +1300

    Add markers and infowindow listeners for all destinations in an array

--- js/map.js
+++ js/map.js
@@ -11,23 +11,52 @@ function initMap() {
         zoom: 11
     });
 
-    //First marker location
-    var chocolate_hills = { lat: 9.831103, lng: 124.139719 };
+    var locationList = [
+        { name: 'Chocolate Hills', location: { lat: 9.798818, lng: 124.167330 } },
+        { name: 'Tarsier Conservation Area', location: { lat: 9.676270, lng: 124.091175 } },
+        { name: 'Loboc Floating Restaurant', location: { lat: 9.635599, lng: 124.03034245 } },
+        { name: 'Hinagdanan Cave', location: { lat: 9.625318, lng: 123.800927 } },
+        { name: 'Panglao Beach', location: { lat: 9.626798, lng: 124.805699 } },
+        { name: 'Balicasag Island', location: { lat: 9.516485, lng: 123.683309 } },
+        { name: 'Baclayon Church', location: { lat: 9.622828, lng: 123.912447 } },
+        { name: 'Python Sanctuary', location: { lat: 9.614213, lng: 123.939295 } },
+        { name: 'Sipatan Twin Hanging Bridge', location: { lat: 9.675264, lng: 124.047947 } },
+        { name: 'Dauis Church', location: { lat: 9.625919, lng: 123.864937 } },
+        { name: 'Bohol Bee Farm', location: { lat: 9.575847, lng: 123.826945 } },
+        { name: 'Blood Compact Shrine', location: { lat: 9.627513, lng: 123.879135 } },
+        { name: 'Butterflies Conservation Center', location: { lat: 9.697643, lng: 124.100070 } },
+        { name: 'Virgin Island', location: { lat: 9.558371, lng: 123.722485 } },
+        { name: 'Shell Museum', location: { lat: 9.580495, lng: 123.748747 } },
+        { name: 'Bilar Man-Made Forest', location: { lat: 9.664285, lng: 124.078474 } },
+        { name: 'Loboc Ecotourism Adventure Park', location: { lat: 9.656049, lng: 124.023793 } }
+    ];
 
-    //Make 1st marker appear
-    var marker = new google.maps.Marker({
-        position: chocolate_hills,
-        map: map,
-        title: 'Chocolate Hills'
-    });
+    // One infoWindow for all markers
+    var infoWindow = new google.maps.InfoWindow();
 
-    // First infoWindow
-    var infoWindow = new google.maps.InfoWindow({
-        content: marker.title
-    });
+    // Create an array of markers from the list of locations
+    var markers = [];
+    for (var i = 0; i < locationList.length; i++) {
+        var marker;
 
-    // Event listener the opens the infoWindow when marker is clicked
-    marker.addListener('click', function () {
-        infoWindow.open(map, marker);
-    });
+        // Push the marker per location in the markers array
+        // Animate by dropping one by one
+        window.setTimeout(function (data) {
+            marker = new google.maps.Marker({
+                map: map,
+                title: data.name,
+                position: data.location,
+                animation: google.maps.Animation.DROP
+            });
+            markers.push(marker);
+
+            // Event listener the opens the infoWindow when marker is clicked
+            marker.addListener('click', (function (markerCopy) {
+                return function () {
+                    infoWindow.setContent(markerCopy.title);
+                    infoWindow.open(map, markerCopy);
+                };
+            })(marker));
+        }, i * 200, locationList[i]);
+    }
 }
