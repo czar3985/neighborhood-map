e74c97d55fe332326a988056ea051ffdc437b74b
Rina Lingad <pixie.czar@gmail.com>
2018-10-20 15:57:51 +1300

    Show first tourist spot

--- js/map.js
+++ js/map.js
@@ -3,11 +3,21 @@
 //Initializes the map
 function initMap() {
      //Creates a new map object.
-     //Bohol, Philippines is at lat: 9.877085, long: 124.133168
+     //Bohol, Philippines is at lat: 9.877085, lng: 124.133168
      //Set center to Bilar, Bohol lat: 9.718134, lng: 124.088440 to better
      //view the tourist spots found mostly at the bottom part of Bohol
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 9.718134, lng: 124.088440 },
         zoom: 11
+    });
+
+    //First marker location
+    var chocolate_hills = { lat: 9.831103, lng: 124.139719 };
+
+    //Make 1st marker appear
+    var marker = new google.maps.Marker({
+        position: chocolate_hills,
+        map: map,
+        title: 'Chocolate Hills'
     });
 }
