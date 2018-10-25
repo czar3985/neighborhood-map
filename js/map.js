62405fc9c81172e1719c2f9dd59e23e112b0d733
Rina Lingad <pixie.czar@gmail.com>
2018-10-26 12:14:28 +1300

    Implement 3rd-party api calls to retrieve data about each location

--- js/map.js
+++ js/map.js
@@ -35,8 +35,7 @@ function initMap() {
             // Event listener the opens the infoWindow when marker is clicked
             marker.addListener('click', (function (markerCopy) {
                 return function () {
-                    infoWindow.setContent(markerCopy.title);
-                    infoWindow.open(map, markerCopy);
+                    setUpInfoWindow(markerCopy);
                 };
             })(marker));
 
@@ -58,4 +57,94 @@ function initMap() {
     window.setTimeout(function () {
         new google.maps.event.trigger(markers[0], 'click');
     }, initialLocations.length * 200);
+}
+
+function setUpInfoWindow(marker) {
+    if (infoWindow.marker !== marker) {
+
+        searchWikipedia(marker);
+        searchFoursquare(marker);
+
+        infoWindow.marker = marker;
+        infoWindow.setContent('<b>' + marker.title + '</b><br /><br />' +
+            'Relevant Wikipedia Articles:<br/><i>Retrieving...</i>' +
+            'Restaurants Nearby:<br /><i>Retrieving...</i>');
+        infoWindow.open(map, marker);
+
+        // Remove associated marker if the infoWindow is closed
+        infoWindow.addListener('closeclick', function () {
+            infoWindow.setMarker = null;
+        });
+    }
+}
+
+var wikiLinks = '<i>None found at this time</i>';
+var restaurantsNearby = '<i>None found at this time</i>';
+
+// Update Info Window
+function updateInfoWindow(marker) {
+    infoWindow.setContent('<b>' + marker.title + '</b><br /><br />' +
+        'Relevant Wikipedia Articles:' +
+        '<ul>' + wikiLinks + '</ul>' +
+        'Restaurants Nearby:' +
+        '<ul>' + restaurantsNearby + '</ul>');
+    infoWindow.open(map, marker);
+}
+
+function searchWikipedia(marker) {
+    var links = '';
+
+    var timeout = setTimeout(function () {
+        wikiLinks = '<i>None found at this time</i>';
+        updateInfoWindow();
+    }, 5000);
+
+    $.ajax({
+        url: 'https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=' +
+            marker.title,
+        dataType: "jsonp",
+        success: function (response) {
+            articles = response[1];
+            $.each(articles, function (index, article) {
+                links += "<li>" + "<a href='https://en.wikipedia.org/wiki/"
+                    + article + "'>" + article + "</a>" + "</li>";
+            });
+            clearTimeout(timeout);
+            if (links === '')
+                links = '<i>None found at this time</i>';
+
+            wikiLinks = links;
+            updateInfoWindow(marker);
+        }
+    });
+}
+
+function searchFoursquare(marker) {
+    var restaurants = '';
+
+    var timeout = setTimeout(function () {
+        restaurantsNearby = '<i>None found at this time</i>';
+        updateInfoWindow();
+    }, 5000);
+
+    $.ajax({
+        url: 'https://api.foursquare.com/v2/venues/search?' +
+            'query=restaurant&limit=5&v=20180323&' +
+            'client_id= BG0BB2V1COPP0FG1BJFGIPT14KAGKTYE4FPKFHIBRKVLDIST&' +
+            'client_secret=PJI0EARPCYSZ32NP5RGYZ3BWKO3R5LGB2CW30BV2QJMKVXIJ&' +
+            'll=' + marker.position.lat() + ',' + marker.position.lng(),
+        dataType: "json",
+        success: function (response) {
+            venues = response['response']['venues'];
+            $.each(venues, function (index, venue) {
+                restaurants += "<li>" + venue.name + "</li>";
+            });
+            clearTimeout(timeout);
+            if (restaurants === '')
+                restaurants = '<i>None found at this time</i>';
+
+            restaurantsNearby = restaurants;
+            updateInfoWindow(marker);
+        }
+    });
 }
